2b80171ae2f6e47a9c5326d39e3838b50f9a8aa2
Axel Yung <axelyung@gmail.com>
2021-09-02 20:48:42 -0700

    1-16

--- tcpl/1-16.c
+++ tcpl/1-16.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 
+#define MAX_LENGTH 100
+
+int getLine(char line[], int maxline);
+void copy(char from[], char to[]);
+
 int main(void) {
-    int blanks, tabs, newlines, other, c;
-    blanks = tabs = newlines = 0;
+    int len, max;
+    char line[MAX_LENGTH], longest[MAX_LENGTH];
 
-    while ((c = getchar()) != EOF) {
-	switch (c) {
-	    case ' ':
-		blanks++;
-		break;
-	    case '\t':
-		tabs++;
-		break;
-	    case '\n':
-		newlines++;
-		break;
-	    default:
-		other++;
+    max = len = 0;
+    do {
+	len = getLine(line, MAX_LENGTH);
+	printf("len = %d\n", len);
+	if (len > max) {
+	    max = len;
+	    printf("new max found: %d\n", max);
+	    copy(line, longest);
 	}
-    }
-
-    printf("blanks: %d\n", blanks);
-    printf("tabs: %d\n", tabs);
-    printf("newlines: %d\n", newlines);
-    printf("other: %d\n", other);
+    } while (len > 0);
+    printf("the longest line was: \"%s\"\n", longest);
+    printf("(%d characters long)\n", max);
     return 0;
 }
+
+int getLine(char line[], int maxline) {
+    int i;
+    for (i = 0; i < MAX_LENGTH; i++) {
+	int c = getchar();
+	if (c == EOF || c == '\n') {
+	    break;
+	}
+	line[i] = c;
+    }
+    line[i + 1] = '\0';
+    return i;
+}
+
+void copy(char from[], char to[]) {
+    int i = 0;
+    int last;
+    do {
+	last = to[i] = from[i];
+	i++;
+    } while (last != '\n');
+}
+
