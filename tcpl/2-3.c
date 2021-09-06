b47f8687c4900d5a44d031748933eabab2c522fa
Axel Yung <axelyung@gmail.com>
2021-09-06 13:31:26 -0700

    print out type lengths

---
+++ tcpl/2-3.c
@@ -0,0 +1,22 @@
+#include <limits.h>
+#include <stdio.h>
+
+#define MAX_LENGTH 1024
+#define TABSPACES 3
+#define TAB 'T'
+#define SPACE 'S'
+
+int get_line(char line[], int maxline);
+
+void entab(char in[], char out[], int len);
+
+int main() {
+    unsigned char uchar_min = 0x00;
+    unsigned char uchar_max = 0xFF;
+    printf("%20s %+8d %+d\n", "unsigned char", uchar_min, uchar_max);
+
+    char char_min = 0b0000001;
+    char char_max = 0b1111111;
+    printf("%20s %8x %x\n", "char", CHAR_BIT, CHAR_MIN);
+}
+
