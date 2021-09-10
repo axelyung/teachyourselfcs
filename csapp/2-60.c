40ee087fa04a7940e56747669b5f0395b1bec035
Axel Yung <axelyung@gmail.com>
2021-09-09 18:09:12 -0700

    2-60

--- csapp/2-60.c
+++ csapp/2-60.c
@@ -4,17 +4,31 @@
 typedef unsigned int uint;
 typedef unsigned int *bp;  // byte pointer
 
-unsigned replace_byte(unsigned x, int i, unsigned char b) { return 0; }
+unsigned replace_byte(unsigned x, int i, unsigned char b);
+
+struct test_case {
+    unsigned x;
+    int i;
+    unsigned char b;
+    unsigned expect;
+};
 
 int main() {
-    printf("%x", ) uint x = 0x89ABCDEF;
-    uint y = 0x76543210;
+    struct test_case cases[3] = {
+	{0x12345678, 2, 0xAB, 0x12AB5678},
+	{0x12345678, 0, 0xAB, 0x123456AB},
+	{0x12345678, 3, 0xAB, 0xAB345678},
+    };
+    for (int i = 0; i < 3; i++) {
+	struct test_case c = cases[i];
+	unsigned result = replace_byte(c.x, c.i, c.b);
+	printf("0x%.8x 0x%.8x\n", result, c.expect);
+    }
+}
 
-    uint lhs = (x & ~0xFF);
-    printf("%x\n", lhs);
-
-    uint rhs = (y & 0xFF);
-    printf("%x\n", rhs);
-
-    printf("%x\n", lhs | rhs);
+unsigned replace_byte(unsigned x, int i, unsigned char b) {
+    unsigned target = 0x000000FF << (i * 8);
+    unsigned zeroed = x & ~target;
+    unsigned replacement = b << (i * 8);
+    return zeroed + replacement;
 }
