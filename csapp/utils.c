e158cd86ebc2dacb44b9234bbdbbf1f6f7bf0abc
Axel Yung <axelyung@gmail.com>
2021-09-10 12:31:34 -0700

    annotate print_binary

--- csapp/utils.c
+++ csapp/utils.c
@@ -3,14 +3,19 @@
 
 void print_binary(size_t const size, void const *const ptr) {
     unsigned char *b = (unsigned char *)ptr;
-    unsigned char byte;
+    unsigned char bit;
     int i, j;
+    // iterate through each byte starting with most significant
     for (i = size - 1; i >= 0; i--) {
+	// iterate through each bit starting with most significant
 	for (j = 7; j >= 0; j--) {
-	    byte = (b[i] >> j) & 1;
-	    printf("%u", byte);
+	    // logical rightshift by j and mask with 1
+	    bit = (b[i] >> j) & 1;
+	    printf("%u", bit);
 	}
+	// print divider between bytes
 	if (i > 0) printf("|");
     }
+    // print new line
     puts("");
 }
